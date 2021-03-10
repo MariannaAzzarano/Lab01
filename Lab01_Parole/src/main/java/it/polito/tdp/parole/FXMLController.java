192c54a825e55c328cb09a6c009d38d4292fd290
Marianna Azzarano <Marianna Azzarano@LAPTOP-54VK15JC.station>
2021-03-10 14:20:49 +0100

    Prima sottomissione

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
@@ -1,8 +1,10 @@
 package it.polito.tdp.parole;
 
+import it.polito.tdp.parole.model.ComparatoreParole;
 import it.polito.tdp.parole.model.Parole;
 
 import java.net.URL;
+import java.util.Collections;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -31,15 +33,44 @@ public class FXMLController {
 
     @FXML
     private Button btnReset;
-
+    
     @FXML
+    private TextField txtTempo;
+
+    @FXML           //DA FARE CONTEGGIO ns
     void doInsert(ActionEvent event) {
-    	// TODO
+    	String nuova_parola = txtParola.getText();        //prendo la parola
+    	elenco.addParola(nuova_parola);                   //inserisco la parola nell'elenco
+    	String stringa = "";                              //inizializzo l'elenco
+    	for(String s : elenco.getElenco()) {              //creo un'elenco in forma stringa
+    		stringa += s +"\n"; 
+    	}
+    	txtResult.setText(stringa);                       //aggiungo la parola (da capo tutto l'elenco)
+    	txtParola.setText("");                            //svuoto la casella di testo dopo aver aggiunto la parola
+    	long ns = System.nanoTime();
+    	txtTempo.setText(""+ns);
+    	
     }
 
-    @FXML
+    @FXML               //DA FARE CONTEGGIO ns
     void doReset(ActionEvent event) {
-    	// TODO
+    	txtResult.setText("");                           //faccio scomparire le parole dall'area di testo
+    	elenco.reset();                                  //svuoto l'elenco
+    	long ns = System.nanoTime();
+    	txtTempo.setText(""+ns);
+    }
+    
+    @FXML              //DA FARE CONTEGGIO ns
+    void doCancella(ActionEvent event) {
+    	elenco.cancella(txtResult.getSelectedText());    //elimino dall'elenco la parola che ho selezionato
+    	String stringa = "";                              //inizializzo l'elenco
+    	for(String s : elenco.getElenco()) {              //creo un'elenco in forma stringa
+    		stringa += s +"\n"; 
+    	}
+    	txtResult.setText(stringa);                       //aggiungo la parola (da capo tutto l'elenco)
+    	long ns = System.nanoTime();
+    	txtTempo.setText(""+ns);
+
     }
 
     @FXML
--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,34 @@
 package it.polito.tdp.parole.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Parole {
+	
+	List<String> listaParole;
 		
 	public Parole() {
-		//TODO
+		listaParole = new LinkedList<String>();     //per fare ultimo punto cambio solo LinkedList o ArrayList!!!
 	}
 	
 	public void addParola(String p) {
-		//TODO
+		listaParole.add(p);
 	}
 	
 	public List<String> getElenco() {
-		//TODO
-		return null;
+		ComparatoreParole c = new ComparatoreParole();
+		Collections.sort(listaParole, c);
+		return listaParole;
 	}
 	
 	public void reset() {
-		// TODO
+		listaParole.clear();
+	}
+	
+	public void cancella(String daCancellare) {
+		listaParole.remove(daCancellare);       //funziona o devo eliminare l'oggetto anzichè la stringa
 	}
 
 }
